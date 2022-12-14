90c09bf8e5b6ff0d845e0691e0de6a8257733041
Ilina Usmanova <ilina@Ilinas-MBP.true.true>
2022-12-14 18:05:14 +0700

    rewritten mongoose connection 3

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -34,4 +27,11 @@ app.use('*', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
