76f5329d0a934ecd0272c7ff1afbf45b46e5a6b7
Ilina Usmanova <ilina@Ilinas-MBP.realtek>
2023-01-12 17:56:10 +0800

    cors issues 6

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,24 +1,25 @@
-const allowedCors = [
-  'https://moe-mesto.nomoredomains.club',
-  'http://moe-mesto.nomoredomains.club',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'localhost:3000',
-];
+// const allowedCors = [
+//   'https://moe-mesto.nomoredomains.club',
+//   'http://moe-mesto.nomoredomains.club',
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+//   'localhost:3000',
+// ];
 
 // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const corsFunction = (req, res, next) => {
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   // const { method } = req;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  // if (allowedCors.includes(origin)) {
+
     // if (method === 'OPTIONS') {
     //   const requestHeaders = req.headers['access-control-request-headers'];
     //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     //   res.header('Access-Control-Allow-Headers', requestHeaders);
     // }
-  }
+  // }
 
   next();
 };
