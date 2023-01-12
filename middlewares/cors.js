07088bcbb515ecd847de3761564894d589dc37e4
Ilina Usmanova <ilina@Ilinas-MBP.realtek>
2023-01-12 15:29:54 +0800

    cors issues 2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,18 +6,18 @@ const allowedCors = [
   'localhost:3000',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const corsFunction = (req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
+  // const { method } = req;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
-    if (method === 'OPTIONS') {
-      const requestHeaders = req.headers['access-control-request-headers'];
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-    }
+    // if (method === 'OPTIONS') {
+    //   const requestHeaders = req.headers['access-control-request-headers'];
+    //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    //   res.header('Access-Control-Allow-Headers', requestHeaders);
+    // }
   }
 
   next();
