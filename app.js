c97ad94f221b1892af61b2a14b95fdf76f5422c6
Ilina Usmanova <ilina@Ilinas-MBP.realtek>
2023-01-12 19:23:36 +0800

    cors issues 11

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ app.use(bodyParser.json());
 app.use(requestLogger);
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', ['*']);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
