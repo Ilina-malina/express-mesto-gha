ca913b8c7fa0a7ed8bb70872be5312d096a76f88
Ilina Usmanova <ilina@Ilinas-MBP.realtek>
2023-01-12 20:12:02 +0800

    123

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(requestLogger);
 
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
